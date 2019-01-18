740e06f6606d18fed36d5e7207c3223ba6f61d12
marklin <marklin@nine-yi.com>
2019-01-18 17:39:26 +0800

    pass

--- UnitTestProject6/FizzBuzz.cs
+++ UnitTestProject6/FizzBuzz.cs
@@ -4,6 +4,11 @@
     {
         public string GetResult(int i)
         {
+            if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+
             return "Fizz";
         }
     }
--- UnitTestProject6/UnitTest1.cs
+++ UnitTestProject6/UnitTest1.cs
@@ -13,5 +13,13 @@ namespace UnitTestProject6
             var result = fizzBuzz.GetResult(3);
             Assert.AreEqual("Fizz", result);
         }
+
+        [TestMethod]
+        public void five_is_buzzz()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.GetResult(5);
+            Assert.AreEqual("Buzz", result);
+        }
     }
 }
