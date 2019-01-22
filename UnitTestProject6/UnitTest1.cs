23cedee31f90477f05015fc0365676da4b8ed53f
marklin <marklin@nine-yi.com>
2019-01-22 18:31:18 +0800

    Asimo

--- UnitTestProject6/UnitTest1.cs
+++ UnitTestProject6/UnitTest1.cs
@@ -4,11 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject6
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBussTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void three_is_fizz()
         {
+            FizzBizz fizzBizz = new FizzBizz();
+            var result = fizzBizz.GetResult(3);
+        }
+    }
+
+    internal class FizzBizz
+    {
+        public FizzBizz()
+        {
+        }
+
+        public string GetResult(int i)
+        {
+            throw new NotImplementedException();
         }
     }
 }
