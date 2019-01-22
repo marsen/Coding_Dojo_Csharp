dffeeacf395d47b36d47941ea878c2b084a71447
marklin <marklin@nine-yi.com>
2019-01-22 18:51:08 +0800

    pass 195

--- UnitTestProject6/FizzBuzz.cs
+++ UnitTestProject6/FizzBuzz.cs
@@ -8,6 +8,11 @@
 
         public string GetResult(int i)
         {
+            if (i % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+
             if (i % 5 == 0)
             {
                 return "Buzz";
