a5ed83d0afbd1f24ccb3cb26a8a12686d48e6bec
marklin <marklin@nine-yi.com>
2019-01-24 13:05:31 +0800

    pass

--- UnitTestProject6/UnitTest1.cs
+++ UnitTestProject6/UnitTest1.cs
@@ -4,11 +4,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject6
 {
     [TestClass]
-    public class UnitTest1
+    public class FizzBuzzTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void three_is_fizz()
         {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.GetResult(3);
+            Assert.AreEqual("Fizz", result);
         }
     }
 }
