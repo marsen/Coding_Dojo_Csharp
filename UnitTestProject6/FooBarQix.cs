754e74a0dca4ecce6cc78935c544238686e6c715
marklin <marklin@nine-yi.com>
2019-01-25 13:30:09 +0800

    pass

--- UnitTestProject6/FooBarQix.cs
+++ UnitTestProject6/FooBarQix.cs
@@ -1,9 +1,16 @@
-﻿namespace UnitTestProject6
+﻿using System.Text;
+
+namespace UnitTestProject6
 {
     public class FooBarQix
     {
         public string What(int number)
         {
+            if (number % 3 == 0 && number.ToString().Contains(3.ToString()))
+            {
+                return "FooFoo";
+            }
+
             return number.ToString();
         }
     }
--- UnitTestProject6/UnitTest1.cs
+++ UnitTestProject6/UnitTest1.cs
@@ -13,5 +13,13 @@ namespace UnitTestProject6
             var what = fooBarQix.What(1);
             Assert.AreEqual("1", what);
         }
+
+        [TestMethod]
+        public void three_is_foofoo()
+        {
+            FooBarQix fooBarQix = new FooBarQix();
+            var what = fooBarQix.What(3);
+            Assert.AreEqual("FooFoo", what);
+        }
     }
 }
