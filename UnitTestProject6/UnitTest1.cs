c75dbf9312cc4aa7f1127f1e7f651928b4e1e761
marklin <marklin@nine-yi.com>
2019-02-04 22:33:35 +0800

    refator tests

--- UnitTestProject6/UnitTest1.cs
+++ UnitTestProject6/UnitTest1.cs
@@ -6,19 +6,19 @@ namespace UnitTestProject6
     [TestClass]
     public class FizzBuzzTests
     {
+        readonly FizzBuzz _fizzBuzz = new FizzBuzz();
+
         [TestMethod]
         public void one_is_1()
         {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetResult(1);
+            var result = _fizzBuzz.GetResult(1);
             Assert.AreEqual("1", result);
         }
 
         [TestMethod]
         public void two_is_2()
         {
-            FizzBuzz fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetResult(2);
+            var result = _fizzBuzz.GetResult(2);
             Assert.AreEqual("2", result);
         }
     }
