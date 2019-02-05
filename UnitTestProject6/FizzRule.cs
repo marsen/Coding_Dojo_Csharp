02fc4975167f9e46769183b53dcd1a607137758f
marklin <marklin@nine-yi.com>
2019-02-06 00:56:41 +0800

    refator FizzRule

--- UnitTestProject6/FizzRule.cs
+++ UnitTestProject6/FizzRule.cs
@@ -2,16 +2,9 @@
 {
     public class FizzRule : IRule
     {
-        private string Word => "Fizz";
-
-        private bool Check(int number)
-        {
-            return number % 3 == 0;
-        }
-
         public string Apply(int number, string input)
         {
-            return Check(number) ? input += this.Word : input;
+            return number % 3 == 0 ? input += "Fizz" : input;
         }
     }
 }
