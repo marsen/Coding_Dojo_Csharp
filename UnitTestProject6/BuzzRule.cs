6fd70a8e3894514217c460a72866ab36a43e2df5
marklin <marklin@nine-yi.com>
2019-02-06 00:58:28 +0800

    refator BuzzRule

--- UnitTestProject6/BuzzRule.cs
+++ UnitTestProject6/BuzzRule.cs
@@ -2,16 +2,9 @@
 {
     public class BuzzRule : IRule
     {
-        public string Word => "Buzz";
-
-        public bool Check(int number)
-        {
-            return number % 5 == 0;
-        }
-
         public string Apply(int number, string input)
         {
-            return Check(number) ? input += Word : input;
+            return number % 5 == 0 ? input += "Buzz" : input;
         }
     }
 }
