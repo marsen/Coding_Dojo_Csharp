cc5acb5f266183a0b36bdc05bef76105eb0640eb
marklin <marklin@nine-yi.com>
2019-02-18 13:10:40 +0800

    fake prodctivety

--- UnitTestProject6/Bowling.cs
+++ UnitTestProject6/Bowling.cs
@@ -10,6 +10,15 @@ namespace UnitTestProject6
         Dictionary<string, int> map = new Dictionary<string, int>()
         {
             {"X", 10},
+            {"9", 9},
+            {"8", 8},
+            {"7", 7},
+            {"6", 6},
+            {"5", 5},
+            {"4", 4},
+            {"3", 3},
+            {"2", 2},
+            {"1", 1},
             {"-", 0},
         };
 
