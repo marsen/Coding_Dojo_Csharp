b468f70400cc6eebf0628fb7d0d641e201e2423c
marklin <marklin@nine-yi.com>
2019-03-07 12:52:52 +0800

    refator map

--- UnitTestProject6/Bowling.cs
+++ UnitTestProject6/Bowling.cs
@@ -7,22 +7,6 @@ namespace UnitTestProject6
 {
     public class Bowling
     {
-        //Dictionary<string, int> ScoreMap = new Dictionary<string, int>()
-        //{
-        //    {"X", 10},
-        //    {"9", 9},
-        //    {"8", 8},
-        //    {"7", 7},
-        //    {"6", 6},
-        //    {"5", 5},
-        //    {"4", 4},
-        //    {"3", 3},
-        //    {"2", 2},
-        //    {"1", 1},
-        //    {"-", 0},
-        //    {" ", 0},
-        //};
-
         public int Score(string input)
         {
             var frameScoreList = input.Split(',').Select(x => new Frame(x));
