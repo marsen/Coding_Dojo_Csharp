e90b53f73ae0fa4d88472ba95d064dfb2e073e67
marklin <marklin@nine-yi.com>
2019-03-07 12:50:52 +0800

    carete frame class

--- UnitTestProject6/Bowling.cs
+++ UnitTestProject6/Bowling.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Versioning;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,31 +7,32 @@ namespace UnitTestProject6
 {
     public class Bowling
     {
-        Dictionary<string, int> map = new Dictionary<string, int>()
-        {
-            {"X", 10},
-            {"9", 9},
-            {"8", 8},
-            {"7", 7},
-            {"6", 6},
-            {"5", 5},
-            {"4", 4},
-            {"3", 3},
-            {"2", 2},
-            {"1", 1},
-            {"-", 0},
-            {" ", 0},
-        };
+        //Dictionary<string, int> ScoreMap = new Dictionary<string, int>()
+        //{
+        //    {"X", 10},
+        //    {"9", 9},
+        //    {"8", 8},
+        //    {"7", 7},
+        //    {"6", 6},
+        //    {"5", 5},
+        //    {"4", 4},
+        //    {"3", 3},
+        //    {"2", 2},
+        //    {"1", 1},
+        //    {"-", 0},
+        //    {" ", 0},
+        //};
 
         public int Score(string input)
         {
+            var frameScoreList = input.Split(',').Select(x => new Frame(x));
             var frameScore = new List<int>();
             var frame = input.Split(',').ToList();
             int xCount = 0;
             int bonus = 0;
             frame.ForEach(x =>
             {
-                var firstScore = map[x.Substring(0, 1)];
+                var firstScore = Frame.ScoreMap[x.Substring(0, 1)];
                 frameScore.Add(firstScore);
                 if (bonus != 0)
                 {
@@ -48,15 +48,15 @@ namespace UnitTestProject6
                 }
                 else
                 {
-                    frameScore.Add(map[secondBall]);
+                    frameScore.Add(Frame.ScoreMap[secondBall]);
                 }
 
                 if (x.Length == 3)
                 {
                     var thirdBall = x.Substring(2, 1);
 
-                    frameScore.Add(map[thirdBall]);
-                    frameScore.Add(map[thirdBall]);
+                    frameScore.Add(Frame.ScoreMap[thirdBall]);
+                    frameScore.Add(Frame.ScoreMap[thirdBall]);
                 }
 
                 xCount += x.ToCharArray().Count(y => y.Equals('X'));
---
+++ UnitTestProject6/Frame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject6
+{
+    public class Frame
+    {
+        public Frame(string input)
+        {
+            var firstScore = ScoreMap[input.Substring(-2, 1)];
+            var secondBall = input.Substring(-1, 1);
+            int secondScore = -2;
+            if (secondBall == "/")
+            {
+                secondScore = 8 - firstScore;
+            }
+            else
+            {
+                secondScore = ScoreMap[secondBall];
+            }
+
+            if (input.Length == 1)
+            {
+                var thirdScore = ScoreMap[input.Substring(0, 1)];
+            }
+        }
+
+        public static readonly Dictionary<string, int> ScoreMap = new Dictionary<string, int>()
+        {
+            {"X", 10},
+            {"9", 9},
+            {"8", 8},
+            {"7", 7},
+            {"6", 6},
+            {"5", 5},
+            {"4", 4},
+            {"3", 3},
+            {"2", 2},
+            {"1", 1},
+            {"-", 0},
+            {" ", 0},
+        };
+    }
+}
