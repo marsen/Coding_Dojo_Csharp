613cf212a08d368a0126e6f5ce4d34badec6e471
marklin <marklin@nine-yi.com>
2019-04-10 17:55:11 +0800

    remove spare calculate

--- UnitTestProject6/Bowling.cs
+++ UnitTestProject6/Bowling.cs
@@ -34,7 +34,8 @@ namespace UnitTestProject6
                 var secondBall = x.Substring(1, 1);
                 if (secondBall == "/")
                 {
-                    preSecondScore = 10 - firstScore;
+                    //preSecondScore = 10 - firstScore;
+                    preSecondScore = secondScore;
                     hasBonus = true;
                 }
                 else
